ed8c3466eea664654f2a5ec5ff72b1b596eb174a
MarkusOhADOP <83572705+MarkusOhADOP@users.noreply.github.com>
2025-02-25 14:27:07 +0900

    [모비위드] 쇼핑몰 광고 테스트 추가

---
+++ netlify/functions/mobwith-ad-test-02.js
@@ -0,0 +1,67 @@
+export const handler = async () => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "SUCCESS",
+      data: {
+        zone: "10887867",
+        adType: "SR",
+        logoSub: "",
+        logoUrl: "https://cdn-f.adsmoloco.com/moloco-cdn/privacy.html",
+        memo: null,
+        target: "mobmixer",
+        data: [
+          {
+            custom09: "https://bidmad-test-server.netlify.app?checker=click",
+            aLogoDefault:
+              "https://mixer.mobon.net/view?seq=3f23dd75-d9c2-4120-b3d8-a09a03703eb1&viewType=LOGO",
+            descWeb: "",
+            custom02: "",
+            aNm: "강남언니",
+            aDesc1: "리얼 후기 220만건 확인",
+            pDcPrice: "",
+            custom06: "",
+            pCode: "something",
+            aTitle: "강남언니",
+            aDesc3: "리얼 후기 220만건 확인",
+            pPrice: "123123123123123123123123123123123123123123",
+            custom03: "",
+            custom07: "리얼 후기 220만건 확인",
+            custom10: "",
+            pNm: "강남언니",
+            custom04: "",
+            aLogoSub:
+              "https://mixer.mobon.net/view?seq=3f23dd75-d9c2-4120-b3d8-a09a03703eb1&viewType=LOGO",
+            aUrl: "https://mixer.mobon.net/click?sspNo=2539&dspTpCode=11&pltfomTpCode=03&userId=adserver&ad_url=https%3A%2F%2Ftr-asia.adsmoloco.com%2Frtb%2Fclick%3Fexchange%3DTPMN%26imp_id%3Dde939800-8ec2-46af-a8c3-8b1ef4576f96%26info%3DChBDUKr1FdRAkLs2OTlZYz66EM-P9b0GGhQIAhoQZcA2rz9NTryHmHdcROnv4iACKgAyAA%26campaign_name%3DanZfiljZBgczQMv6_Biattributable%26dcr%3D&dspClick=https%3A%2F%2Fasia-seoul-statistics.tpmn.io%2Fmatclick.tpmn%3Fai%3D2539_960571015%2528APP%2529%26pi%3D27%26adid%3D65C036AF-3F4D-4EBC-8798-775C44E9EFE2%26dspid%3D129%26sspid%3D34%26hashid%3Dd7f08fe7d4846bf5cbbf35dca6c2169a%26bidid%3Dde939800-8ec2-46af-a8c3-8b1ef4576f96%26timestamp%3D1740457935%26tagid%3DD_static%26tsid%3D38189effc22547c787a76e2e3162d4d11740457935%26appbundle%3D960571015%26adtypecd%3D2502%26chtypecd%3D3501%26aw%3D0%26ah%3D0%26mid%3Dee2cdbcfc0dd61058362e44029799bfb%26clkloc%3D%26scurl%3D%26tu%3D",
+            dcRate: 0,
+            custom08: "https://bidmad-test-server.netlify.app?checker=imgimp",
+            aDesc2: "리얼 후기 220만건 확인",
+            remarks: "",
+            pImgBkg: "",
+            pUrl: "https://mixer.mobon.net/click?sspNo=2539&dspTpCode=11&pltfomTpCode=03&userId=adserver&ad_url=https%3A%2F%2Ftr-asia.adsmoloco.com%2Frtb%2Fclick%3Fexchange%3DTPMN%26imp_id%3Dde939800-8ec2-46af-a8c3-8b1ef4576f96%26info%3DChBDUKr1FdRAkLs2OTlZYz66EM-P9b0GGhQIAhoQZcA2rz9NTryHmHdcROnv4iACKgAyAA%26campaign_name%3DanZfiljZBgczQMv6_Biattributable%26dcr%3D&dspClick=https%3A%2F%2Fasia-seoul-statistics.tpmn.io%2Fmatclick.tpmn%3Fai%3D2539_960571015%2528APP%2529%26pi%3D27%26adid%3D65C036AF-3F4D-4EBC-8798-775C44E9EFE2%26dspid%3D129%26sspid%3D34%26hashid%3Dd7f08fe7d4846bf5cbbf35dca6c2169a%26bidid%3Dde939800-8ec2-46af-a8c3-8b1ef4576f96%26timestamp%3D1740457935%26tagid%3DD_static%26tsid%3D38189effc22547c787a76e2e3162d4d11740457935%26appbundle%3D960571015%26adtypecd%3D2502%26chtypecd%3D3501%26aw%3D0%26ah%3D0%26mid%3Dee2cdbcfc0dd61058362e44029799bfb%26clkloc%3D%26scurl%3D%26tu%3D",
+            desc: "",
+            custom01: "",
+            pImg: "https://mixer.mobon.net/view?seq=3f23dd75-d9c2-4120-b3d8-a09a03703eb1&viewType=IMG",
+            custom05: "",
+            mobile: {
+              mImg320_480: "",
+              mImg320_50: "",
+              mImg320_100: "",
+              mImg250_250: "",
+              mImg160_600: "",
+              mImg300_250: "",
+              mImg120_600: "",
+              mImg728_90: "",
+            },
+          },
+        ],
+        impUrl: "https://bidmad-test-server.netlify.app?checker=imp",
+        adCnt: 1,
+        logoDefault: "https://bidmad-test-server.netlify.app/info.circle.png",
+        siteUrl:
+          "https://tr-asia.adsmoloco.com/rtb/click?exchange=TPMN&imp_id=de939800-8ec2-46af-a8c3-8b1ef4576f96&info=ChBDUKr1FdRAkLs2OTlZYz66EM-P9b0GGhQIAhoQZcA2rz9NTryHmHdcROnv4iACKgAyAA&campaign_name=anZfiljZBgczQMv6_Biattributable&dcr=",
+      },
+      code: "0000",
+    }),
+  };
+};
