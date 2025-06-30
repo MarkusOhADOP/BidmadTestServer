44bf32d76ad459c5fbe4b46e00efdf2cd54fd2ef
Markus Oh <83572705+MarkusOhADOP@users.noreply.github.com>
2025-06-30 16:40:47 +0900

    Update revive-test1.js

--- netlify/functions/revive-test1.js
+++ netlify/functions/revive-test1.js
@@ -15,8 +15,8 @@ const response = {
             * { padding:0px; margin:0px; }
             a { text-decoration:none; color:#313534; line-height: 0; }
             html, body{
-                width: 100%;
-                height: 250px;
+                width: 320px;
+                height: 50px;
                 display: flex;
                 justify-content: center;
                 align-items: center;
