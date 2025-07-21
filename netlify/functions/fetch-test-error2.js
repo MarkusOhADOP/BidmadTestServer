964227155300363c22d6e8a53d1176eb8c89a3a9
Markus Oh <83572705+MarkusOhADOP@users.noreply.github.com>
2025-07-21 14:02:54 +0900

    Create fetch-test-error2.js

---
+++ netlify/functions/fetch-test-error2.js
@@ -0,0 +1,12 @@
+export const handler = async () => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      command: "ok",
+      isFBB: "N",
+      status: "200",
+      type: "v",
+      ads: {},
+    }),
+  };
+};
