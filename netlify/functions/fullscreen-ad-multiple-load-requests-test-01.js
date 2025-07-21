d7061276055d91be4447bdfde5e05341ec83cbe0
Markus Oh <83572705+MarkusOhADOP@users.noreply.github.com>
2025-07-21 14:51:46 +0900

    Create fullscreen-ad-multiple-load-requests-test-01.js

---
+++ netlify/functions/fullscreen-ad-multiple-load-requests-test-01.js
@@ -0,0 +1,26 @@
+export const handler = async () => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      command: "ok",
+      isFBB: "N",
+      status: "200",
+      type: "reward",
+      ads: {
+        "898b5c62-13b7-4004-9847-fc084559051b": {
+          adcode: "fullscreen-ad-multiple-load-requests-test-01",
+          pubid: "",
+          passback: "",
+          adtype: "testtest-test-test-test-testtesttest",
+          advidx: "09316c92-262a-463f-a73b-1f1c34a6de91",
+          areaidx: "898b5c62-13b7-4004-9847-fc084559051b",
+          adweight: "100",
+          adv_price: "10",
+          order: "12",
+          width: "720",
+          height: "1230",
+        },
+      },
+    }),
+  };
+};
