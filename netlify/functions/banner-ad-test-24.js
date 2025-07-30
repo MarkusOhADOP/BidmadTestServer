dda508e4a8accba7f46edbe9474976257179833e
Markus Oh <83572705+MarkusOhADOP@users.noreply.github.com>
2025-07-30 14:17:50 +0900

    배너 테스트 케이스 추가

---
+++ netlify/functions/banner-ad-test-24.js
@@ -0,0 +1,25 @@
+export const handler = async () => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      command: "ok",
+      isFBB: "N",
+      status: "200",
+      ads: {
+        "aa0b053d-e286-44a6-866d-000000000001": {
+          adcode: "ca-app-pub-3940256099942544/2435281174",
+          pubid: "",
+          passback: "0f271960-8c75-4ccb-9d68-18c2272b8056",
+          adtype: "malformed-adtype",
+          advidx: "23503470-4e23-4a84-a77b-a9fe25deb784",
+          areaidx: "aa0b053d-e286-44a6-866d-f754bb7315ae",
+          adweight: "100",
+          adv_price: "1",
+          order: "1",
+          width: "320",
+          height: "50",
+        },
+      },
+    }),
+  };
+};
