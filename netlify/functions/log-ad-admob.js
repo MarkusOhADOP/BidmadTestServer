6363143697675385069345ed6193c0c3a99fb4bc
Markus Oh <83572705+MarkusOhADOP@users.noreply.github.com>
2025-08-05 16:43:33 +0900

    Create log-ad-admob.js

---
+++ netlify/functions/log-ad-admob.js
@@ -0,0 +1,11 @@
+export const handler = async () => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      command: "ok",
+      isFBB: "N",
+      status: "200",
+      type: "v",
+    }),
+  };
+};
