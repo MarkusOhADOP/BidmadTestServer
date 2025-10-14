5d271e7c975585ce51b8a2e5287001da70573aa2
Markus Oh <83572705+MarkusOhADOP@users.noreply.github.com>
2025-10-14 11:03:52 +0900

    Test Response for Mobwith V2 added

---
+++ netlify/functions/mobwith-ad-v2-test-03.js
@@ -0,0 +1,10 @@
+export const handler = async () => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      code: "1204",
+      message: "NO AD",
+      data: null,
+    }),
+  };
+};
---
+++ netlify/functions/mobwith-ad-v2-test-04.js
@@ -0,0 +1,10 @@
+export const handler = async () => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      code: "9999",
+      message: "FALSE",
+      data: null,
+    }),
+  };
+};
