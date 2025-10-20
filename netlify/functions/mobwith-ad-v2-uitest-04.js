e6d5a9a28968e1937d2c356a1b6921661b6753f3
Markus Oh <83572705+MarkusOhADOP@users.noreply.github.com>
2025-10-20 17:03:32 +0900

    commit

---
+++ netlify/functions/mobwith-ad-v2-uitest-04.js
@@ -0,0 +1,67 @@
+export const handler = async () => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      code: "0000",
+      message: "SUCCESS",
+      data: {
+        zone: "489780",
+        adCnt: 1,
+        externalInfo: {
+          externalId: "mobwithads_fc",
+          logoMainImgUrl: "",
+          logoSubImgUrl: "",
+          logoLandingUrl: "https://mobwith.co.kr/adcheck/",
+          impTrackers: [
+            "https://devdsp.mobwith.co.kr/service/imp?advrtsPrdtCode=01&zone=10889374&ad=b41230fa95074fc5a5662f371afec6c4_2955____0&isTune=false&mo_source=with&mo_medium=10889374&mo_campaign=&mo_content=Gf7S0Dk9j7kSgpRP%2FgRSTK7woJBla8oEklVqsJ2OmW74x%2BHdt95H622vXyjIOHgEVWE1d6jlZosubLW8y%2F4F1cv61J2vOeBwzNFYO6lVjtHKr7vsAwFrRw%3D%3D",
+          ],
+        },
+        advertInfo: [
+          {
+            advertiserInfo: {
+              aSiteName: "MOBWITH ADS 마스터",
+              aSiteUrl: "https://www.mobwithad.com",
+              aSiteTitle: "MOBWITH ADS 마스터",
+              aLogoMainImgUrl: "",
+              aLogoSubImgUrl: "",
+            },
+            adType: "banner",
+            adPrdtYn: "N",
+            adVideoUrl: "",
+            adImgUrl:
+              "https://devdsp.mobwith.co.kr/service/image?advrtsPrdtCode=01&zone=10889374&ad=b41230fa95074fc5a5662f371afec6c4_2955____0&isTune=false&mo_source=with&mo_medium=10889374&mo_campaign=&mo_content=Gf7S0Dk9j7kSgpRP%2FgRSTK7woJBla8oEklVqsJ2OmW74x%2BHdt95H622vXyjIOHgEVWE1d6jlZosubLW8y%2F4F1cv61J2vOeBwzNFYO6lVjtHKr7vsAwFrRw%3D%3D",
+            adImgUrlInfo: {
+              adImg120_600: "",
+              adImg160_600: "",
+              adImg250_250: "",
+              adImg300_250:
+                "https://devdsp.mobwith.co.kr/service/image?advrtsPrdtCode=01&zone=10889374&ad=b41230fa95074fc5a5662f371afec6c4_2955____0&isTune=false&mo_source=with&mo_medium=10889374&mo_campaign=&mo_content=Gf7S0Dk9j7kSgpRP%2FgRSTK7woJBla8oEklVqsJ2OmW74x%2BHdt95H622vXyjIOHgEVWE1d6jlZosubLW8y%2F4F1cv61J2vOeBwzNFYO6lVjtHKr7vsAwFrRw%3D%3D",
+              adImg320_50: "",
+              adImg320_100: "",
+              adImg320_480: "",
+              adImg720_120: "",
+              adImg720_1230: "",
+              adImg728_90: "",
+              adImg800_1500: "",
+            },
+            adLandingUrl:
+              "https://devdsp.mobwith.co.kr/service/click?advrtsPrdtCode=01&zone=10889374&ad=b41230fa95074fc5a5662f371afec6c4_2955____0&isTune=false&mo_source=with&mo_medium=10889374&mo_campaign=&mo_content=Gf7S0Dk9j7kSgpRP%2FgRSTK7woJBla8oEklVqsJ2OmW74x%2BHdt95H622vXyjIOHgEVWE1d6jlZosubLW8y%2F4F1cv61J2vOeBwzNFYO6lVjtHKr7vsAwFrRw%3D%3D",
+            adImpTrackers: [
+              "https://devdsp.mobwith.co.kr/service/imp?advrtsPrdtCode=01&zone=10889374&ad=b41230fa95074fc5a5662f371afec6c4_2955____0&isTune=false&mo_source=with&mo_medium=10889374&mo_campaign=&mo_content=Gf7S0Dk9j7kSgpRP%2FgRSTK7woJBla8oEklVqsJ2OmW74x%2BHdt95H622vXyjIOHgEVWE1d6jlZosubLW8y%2F4F1cv61J2vOeBwzNFYO6lVjtHKr7vsAwFrRw%3D%3D",
+            ],
+            adClickTrackers: [],
+            adDesc: "MOBWITH ADS 마스터",
+            productInfo: {
+              pCode: "",
+              pName: "MOBWITH ADS 마스터",
+              pPrice: "",
+              pDcRate: 0,
+              pDcPrice: "",
+              pDeliveryYn: "",
+            },
+          },
+        ],
+      },
+    }),
+  };
+};
