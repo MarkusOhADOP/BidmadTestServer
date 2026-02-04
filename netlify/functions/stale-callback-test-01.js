9187472c33745e72f2fee28c367e5ec8c8d66df6
MarkusOhADOP <83572705+MarkusOhADOP@users.noreply.github.com>
2026-02-04 17:13:18 +0900

    TEST CASE 추가

---
+++ netlify/functions/stale-callback-test-01.js
@@ -0,0 +1,26 @@
+export const handler = async () => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      command: "ok",
+      isFBB: "N",
+      status: "200",
+      type: "banner",
+      ads: {
+        "stale-callback-test-01": {
+          adcode: "stale-callback-test-01",
+          pubid: "",
+          passback: "",
+          adtype: "testtest-test-test-test-testtesttest",
+          advidx: "09316c92-262a-463f-a73b-1f1c34a6de91",
+          areaidx: "stale-callback-test-01",
+          adweight: "100",
+          adv_price: "10",
+          order: "12",
+          width: "320",
+          height: "50",
+        },
+      },
+    }),
+  };
+};
