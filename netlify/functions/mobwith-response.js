5352309d639ca3411340f25d7d9194518a2d97cc
MarkusOhADOP <83572705+MarkusOhADOP@users.noreply.github.com>
2026-02-25 13:29:16 +0900

    Update mobwith-response.js

--- netlify/functions/mobwith-response.js
+++ netlify/functions/mobwith-response.js
@@ -1,79 +1,9 @@
 export const handler = async () => {
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      code: "0000",
-      message: "SUCCESS",
-      data: {
-        zone: "10886412",
-        adCnt: 1,
-        externalInfo: {
-          externalId: "mobmixer",
-          logoMainImgUrl:
-            "https://cr.adsappier.com/ef7b65b3-63b4-4a22-8a1f-7f0a3d83d796/Xl0lrjd_1620370519257_0.png",
-          logoSubImgUrl: "",
-          logoLandingUrl: "https://www.appier.com/ko/privacy-policy/",
-          impTrackers: [],
-        },
-        advertInfo: [
-          {
-            advertiserInfo: {
-              aSiteName: "Freecash",
-              aSiteUrl:
-                "https://krrtb.c.appier.net/xclk?bidobjid=Xc7xoEmEDZqG4aYupGueaQ&cid=kDi5p2lxQ_q0Lc9-VckYvw&crid=nl7jl-CCShKe1cXQbyx6fQ&crpid=VcvwsQFURaaXCt7HexqvXw&soid=NTOr&partner_id=0U8O1JmgRcSOFZ6&bx=Cylxwnu_orZPKqIz7qZy7rMmoP6m7Pu-uYVNuqa1cmA1K4lqU1u_uYNNUYtNZmOYJ4zWUmlEUbI93bgmc8zNJbgBU1u-uYS9uqa1pk4Mpu96HRS5IsvWNwPW6cPFjXj-1XtepwyJplCS_p4x_QnuHPoxo4ay7rd1K4lEUHM12qM-uYSxJ4u_onx1UDo121lNUYtyUmgfu1x1U8A121uVo4u-uYOPJ1u_uqGxKqd9o4QxujR&ui=CylE3b3Nuqa1oq6s7qST20GWoq6m7yRR2I7GKrfQ2GGWoPIsoRiSoPoDMRIxu1x13Ygfo1u_uqGyonQm71Qs71Qy2qaVo4lO&consent=1&ddhh=oqIWod",
-              aSiteTitle: "Freecash - 돈버는앱프리캐시",
-              aLogoMainImgUrl:
-                "https://cr.adsappier.com/i/754cb315-d34c-4d53-afb5-42c995199448/W7NZkB0_1770263768510_0.jpeg",
-              aLogoSubImgUrl:
-                "https://cr.adsappier.com/i/754cb315-d34c-4d53-afb5-42c995199448/W7NZkB0_1770263768510_0.jpeg",
-            },
-            adType: "banner",
-            adPrdtYn: "N",
-            adVideoUrl: "",
-            adImgUrl:
-              "https://cr.adsappier.com/i/f6faac98-fb93-441a-800d-009fd49cf2e6/W7NZkB0_1770263767063_0.jpeg",
-            adImgUrlInfo: {
-              adImg120_600: "",
-              adImg160_600: "",
-              adImg250_250: "",
-              adImg300_250: "",
-              adImg320_50: "",
-              adImg320_100: "",
-              adImg320_480: "",
-              adImg720_120: "",
-              adImg720_1230: "",
-              adImg728_90: "",
-              adImg800_1500: "",
-              adImgCustom: {
-                width: "",
-                height: "",
-                url: "",
-              },
-            },
-            adLandingUrl:
-              "https://krrtb.c.appier.net/xclk?bidobjid=Xc7xoEmEDZqG4aYupGueaQ&cid=kDi5p2lxQ_q0Lc9-VckYvw&crid=nl7jl-CCShKe1cXQbyx6fQ&crpid=VcvwsQFURaaXCt7HexqvXw&soid=NTOr&partner_id=0U8O1JmgRcSOFZ6&bx=Cylxwnu_orZPKqIz7qZy7rMmoP6m7Pu-uYVNuqa1cmA1K4lqU1u_uYNNUYtNZmOYJ4zWUmlEUbI93bgmc8zNJbgBU1u-uYS9uqa1pk4Mpu96HRS5IsvWNwPW6cPFjXj-1XtepwyJplCS_p4x_QnuHPoxo4ay7rd1K4lEUHM12qM-uYSxJ4u_onx1UDo121lNUYtyUmgfu1x1U8A121uVo4u-uYOPJ1u_uqGxKqd9o4QxujR&ui=CylE3b3Nuqa1oq6s7qST20GWoq6m7yRR2I7GKrfQ2GGWoPIsoRiSoPoDMRIxu1x13Ygfo1u_uqGyonQm71Qs71Qy2qaVo4lO&consent=1&ddhh=oqIWod",
-            adImpTrackers: [
-              "https://ss-jp2.appiersig.com/winshowimg?bidobjid=Xc7xoEmEDZqG4aYupGueaQ&cid=kDi5p2lxQ_q0Lc9-VckYvw&crid=nl7jl-CCShKe1cXQbyx6fQ&crpid=VcvwsQFURaaXCt7HexqvXw&soid=NTOr&partner_id=0U8O1JmgRcSOFZ6&bx=Cylxwnu_orZPKqIz7qZy7rMmoP6m7Pu-uYVNuqa1cmA1K4lqU1u_uYNNUYtNZmOYJ4zWUmlEUbI93bgmc8zNJbgBU1u-uYS9uqa1pk4Mpu96HRS5IsvWNwPW6cPFjXj-1XtepwyJplCS_p4x_QnuHPoxo4ay7rd1K4lEUHM12qM-uYSxJ4u_onx1UDo121lNUYtyUmgfu1x1U8A121uVo4u-uYOPJ1u_uqGxKqd9o4QxujR&ui=CylE3b3Nuqa1oq6s7qST20GWoq6m7yRR2I7GKrfQ2GGWoPIsoRiSoPoDMRIxu1x13Ygfo1u_uqGyonQm71Qs71Qy2qaVo4lO&consent=1&ddhh=oqIWod&price=0.01735967254638672",
-              "https://vst.c.appier.net/w?cid=kDi5p2lxQ_q0Lc9-VckYvw&crid=nl7jl-CCShKe1cXQbyx6fQ&crpid=VcvwsQFURaaXCt7HexqvXw&bidobjid=Xc7xoEmEDZqG4aYupGueaQ&partner_id=0U8O1JmgRcSOFZ6&tracking_ns=&consent=1&w=0",
-              "https://mt-usw.appiersig.com/event?bidobjid=Xc7xoEmEDZqG4aYupGueaQ&cid=kDi5p2lxQ_q0Lc9-VckYvw&oid=cJMei3NES9uNdvCZPAkQyA&partner_id=0U8O1JmgRcSOFZ6&s2s=1&event=show&loc=show&dm=&osv=10.0.0.0&adj=1&avd=CylEuqa1oq6s7qST20GWoq6m7yRR2I7GKrfQ2GGWoPIsoRiSoPoDMRIxu1x1Uyu_uqGxu1x1Z4u_uqGyonQm71Qs71QyujR&cn=0cbS93bSPUm3RKYsBwYg-3nzfcH3EUYSRc8O9&url=cLtRZLo_KyOmc8iDKYSfcjiPJ4zqUmRBc8sxZYiPZmgBU1AVJmRyC8OgJqOqw8sxw8gjUqsbMsOTISTltileMIzGHRWnHR7MtiOntiwVHPdRo0uy713N3bJyUDix5rTi2GAVnYsjIY7rrR3c713qZYiNJbgm3rsaw8zfwH7B3jM9U8O1c8VgKYtEJYg9wHtEUmQYUbS138xOcbS93bSPUm3RKYsBwYg-3nzfcH3EUYSRc8O9lYSxZbggZgOqUbgqcsOE30s0C8VVwmzsHDi3iRJqZYgQUsg8IIgErs3Bts3VoYzP2LJWiP7kcgaVnPt-ZiE33qGyo8xzirTk8SIRZPT7ZPJNUYslijJPrjtDcYJt7sfPJLIVC0SDtbVQwmzsHDijoDSkcfEMnIfPcIg3IRQQtPJn7sl23R7oImRQrSaVnPt-CGE13qGyoHiQcHl278OltiZPZmVVnil5wqNVJPSK7bxVi8sYoruVULJDIGEtiilEiDtlJb7cniEnJmgjZsELnjoP2S7RJrSQoiEXIRisZ8GVoqTDUrJ3c83DUIgHUDlkZrJ9IguDob3GnPNLCYO1niJDZq73JmsstbO1J8RDZIEjJ8EnlYJxZsON3bgf5ruQ7rwVwr6VKruQ7qZW70gq34Rz20NNKros7r7g3roP7m7go43RZYSqcmiyHmVEU8gR5ruso0dxo43PojoOon3EZSON3bty3H7P5iWqUbggUjtecHTJljiP3Hlew8JgUjMOr8O_c8V-wnIytqI9o4Iyo4Iy2GVEUjiQlr74lruxM8zfZYOE34Iyo0Gxlr74lruxrGRWtPZVoGQgoqT4J8g-34IytgSKIrG9orfVoquyKqdxo1IPM1IyoLJmlruzlruxMHTxUbiH38lKcHMgofwsoPZ9oPwgoqdgoqNKnSt7r4IyMyIyobVEcmIgoqTL387FUyIy2nIyoS3gZj7EUmQgofwRKqdgoqT0cLlBU8IgofwV70I9o4QD7qoyKqZzlruxr8O1c8VglruxImSYwHlElrlb7roDKqom",
-              "https://mixer.mobon.net/view?sspNo=1897&dspTpCode=18&pltfomTpCode=03&userId=adserver&impId=2deb4fd7-02fa-4ace-9977-f90820c4365c&abusingCode=3JGKSu9jjNZnkTgs6mmioQ&krwPoint=24.699342727661133&point=0.01735967254638672&burl=N",
-            ],
-            adClickTrackers: [
-              "https://mixer.mobon.net/click?sspNo=1897&dspTpCode=18&pltfomTpCode=03&userId=adserver&impId=2deb4fd7-02fa-4ace-9977-f90820c4365c&abusingCode=3JGKSu9jjNZnkTgs6mmioQ",
-            ],
-            adDesc:
-              "리워드앱재택 부업 찾는다면? 무료 앱 프리캐시로 쉽고 재밌게 리워드 받기\n",
-            productInfo: {
-              pCode: "",
-              pName: "Freecash - 돈버는앱프리캐시",
-              pPrice: "",
-              pDcRate: 0,
-              pDcPrice: "",
-              pDeliveryYn: "",
-            },
-          },
-        ],
-      },
-    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: '{"code":"0000","message":"SUCCESS","data":{"zone":"10886412","adCnt":1,"externalInfo":{"externalId":"mobmixer","logoMainImgUrl":"https:\\/\\/cr.adsappier.com\\/ef7b65b3-63b4-4a22-8a1f-7f0a3d83d796\\/Xl0lrjd_1620370519257_0.png","logoSubImgUrl":"","logoLandingUrl":"https:\\/\\/www.appier.com\\/ko\\/privacy-policy\\/","impTrackers":[]},"advertInfo":[{"advertiserInfo":{"aSiteName":"Freecash","aSiteUrl":"https:\\/\\/krrtb.c.appier.net\\/xclk?bidobjid=Xc7xoEmEDZqG4aYupGueaQ&cid=kDi5p2lxQ_q0Lc9-VckYvw&crid=nl7jl-CCShKe1cXQbyx6fQ&crpid=VcvwsQFURaaXCt7HexqvXw&soid=NTOr&partner_id=0U8O1JmgRcSOFZ6&bx=Cylxwnu_orZPKqIz7qZy7rMmoP6m7Pu-uYVNuqa1cmA1K4lqU1u_uYNNUYtNZmOYJ4zWUmlEUbI93bgmc8zNJbgBU1u-uYS9uqa1pk4Mpu96HRS5IsvWNwPW6cPFjXj-1XtepwyJplCS_p4x_QnuHPoxo4ay7rd1K4lEUHM12qM-uYSxJ4u_onx1UDo121lNUYtyUmgfu1x1U8A121uVo4u-uYOPJ1u_uqGxKqd9o4QxujR&ui=CylE3b3Nuqa1oq6s7qST20GWoq6m7yRR2I7GKrfQ2GGWoPIsoRiSoPoDMRIxu1x13Ygfo1u_uqGyonQm71Qs71Qy2qaVo4lO&consent=1&ddhh=oqIWod","aSiteTitle":"Freecash - 돈버는앱프리캐시","aLogoMainImgUrl":"https:\\/\\/cr.adsappier.com\\/i\\/754cb315-d34c-4d53-afb5-42c995199448\\/W7NZkB0_1770263768510_0.jpeg","aLogoSubImgUrl":"https:\\/\\/cr.adsappier.com\\/i\\/754cb315-d34c-4d53-afb5-42c995199448\\/W7NZkB0_1770263768510_0.jpeg"},"adType":"banner","adPrdtYn":"N","adVideoUrl":"","adImgUrl":"https:\\/\\/cr.adsappier.com\\/i\\/f6faac98-fb93-441a-800d-009fd49cf2e6\\/W7NZkB0_1770263767063_0.jpeg","adImgUrlInfo":{"adImg120_600":"","adImg160_600":"","adImg250_250":"","adImg300_250":"","adImg320_50":"","adImg320_100":"","adImg320_480":"","adImg720_120":"","adImg720_1230":"","adImg728_90":"","adImg800_1500":"","adImgCustom":{"width":"","height":"","url":""}},"adLandingUrl":"https:\\/\\/krrtb.c.appier.net\\/xclk?bidobjid=Xc7xoEmEDZqG4aYupGueaQ&cid=kDi5p2lxQ_q0Lc9-VckYvw&crid=nl7jl-CCShKe1cXQbyx6fQ&crpid=VcvwsQFURaaXCt7HexqvXw&soid=NTOr&partner_id=0U8O1JmgRcSOFZ6&bx=Cylxwnu_orZPKqIz7qZy7rMmoP6m7Pu-uYVNuqa1cmA1K4lqU1u_uYNNUYtNZmOYJ4zWUmlEUbI93bgmc8zNJbgBU1u-uYS9uqa1pk4Mpu96HRS5IsvWNwPW6cPFjXj-1XtepwyJplCS_p4x_QnuHPoxo4ay7rd1K4lEUHM12qM-uYSxJ4u_onx1UDo121lNUYtyUmgfu1x1U8A121uVo4u-uYOPJ1u_uqGxKqd9o4QxujR&ui=CylE3b3Nuqa1oq6s7qST20GWoq6m7yRR2I7GKrfQ2GGWoPIsoRiSoPoDMRIxu1x13Ygfo1u_uqGyonQm71Qs71Qy2qaVo4lO&consent=1&ddhh=oqIWod","adImpTrackers":["https:\\/\\/ss-jp2.appiersig.com\\/winshowimg?bidobjid=Xc7xoEmEDZqG4aYupGueaQ&cid=kDi5p2lxQ_q0Lc9-VckYvw&crid=nl7jl-CCShKe1cXQbyx6fQ&crpid=VcvwsQFURaaXCt7HexqvXw&soid=NTOr&partner_id=0U8O1JmgRcSOFZ6&bx=Cylxwnu_orZPKqIz7qZy7rMmoP6m7Pu-uYVNuqa1cmA1K4lqU1u_uYNNUYtNZmOYJ4zWUmlEUbI93bgmc8zNJbgBU1u-uYS9uqa1pk4Mpu96HRS5IsvWNwPW6cPFjXj-1XtepwyJplCS_p4x_QnuHPoxo4ay7rd1K4lEUHM12qM-uYSxJ4u_onx1UDo121lNUYtyUmgfu1x1U8A121uVo4u-uYOPJ1u_uqGxKqd9o4QxujR&ui=CylE3b3Nuqa1oq6s7qST20GWoq6m7yRR2I7GKrfQ2GGWoPIsoRiSoPoDMRIxu1x13Ygfo1u_uqGyonQm71Qs71Qy2qaVo4lO&consent=1&ddhh=oqIWod&price=0.01735967254638672","https:\\/\\/vst.c.appier.net\\/w?cid=kDi5p2lxQ_q0Lc9-VckYvw&crid=nl7jl-CCShKe1cXQbyx6fQ&crpid=VcvwsQFURaaXCt7HexqvXw&bidobjid=Xc7xoEmEDZqG4aYupGueaQ&partner_id=0U8O1JmgRcSOFZ6&tracking_ns=&consent=1&w=0","https:\\/\\/mt-usw.appiersig.com\\/event?bidobjid=Xc7xoEmEDZqG4aYupGueaQ&cid=kDi5p2lxQ_q0Lc9-VckYvw&oid=cJMei3NES9uNdvCZPAkQyA&partner_id=0U8O1JmgRcSOFZ6&s2s=1&event=show&loc=show&dm=&osv=10.0.0.0&adj=1&avd=CylEuqa1oq6s7qST20GWoq6m7yRR2I7GKrfQ2GGWoPIsoRiSoPoDMRIxu1x1Uyu_uqGxu1x1Z4u_uqGyonQm71Qs71QyujR&cn=0cbS93bSPUm3RKYsBwYg-3nzfcH3EUYSRc8O9&url=cLtRZLo_KyOmc8iDKYSfcjiPJ4zqUmRBc8sxZYiPZmgBU1AVJmRyC8OgJqOqw8sxw8gjUqsbMsOTISTltileMIzGHRWnHR7MtiOntiwVHPdRo0uy713N3bJyUDix5rTi2GAVnYsjIY7rrR3c713qZYiNJbgm3rsaw8zfwH7B3jM9U8O1c8VgKYtEJYg9wHtEUmQYUbS138xOcbS93bSPUm3RKYsBwYg-3nzfcH3EUYSRc8O9lYSxZbggZgOqUbgqcsOE30s0C8VVwmzsHDi3iRJqZYgQUsg8IIgErs3Bts3VoYzP2LJWiP7kcgaVnPt-ZiE33qGyo8xzirTk8SIRZPT7ZPJNUYslijJPrjtDcYJt7sfPJLIVC0SDtbVQwmzsHDijoDSkcfEMnIfPcIg3IRQQtPJn7sl23R7oImRQrSaVnPt-CGE13qGyoHiQcHl278OltiZPZmVVnil5wqNVJPSK7bxVi8sYoruVULJDIGEtiilEiDtlJb7cniEnJmgjZsELnjoP2S7RJrSQoiEXIRisZ8GVoqTDUrJ3c83DUIgHUDlkZrJ9IguDob3GnPNLCYO1niJDZq73JmsstbO1J8RDZIEjJ8EnlYJxZsON3bgf5ruQ7rwVwr6VKruQ7qZW70gq34Rz20NNKros7r7g3roP7m7go43RZYSqcmiyHmVEU8gR5ruso0dxo43PojoOon3EZSON3bty3H7P5iWqUbggUjtecHTJljiP3Hlew8JgUjMOr8O_c8V-wnIytqI9o4Iyo4Iy2GVEUjiQlr74lruxM8zfZYOE34Iyo0Gxlr74lruxrGRWtPZVoGQgoqT4J8g-34IytgSKIrG9orfVoquyKqdxo1IPM1IyoLJmlruzlruxMHTxUbiH38lKcHMgofwsoPZ9oPwgoqdgoqNKnSt7r4IyMyIyobVEcmIgoqTL387FUyIy2nIyoS3gZj7EUmQgofwRKqdgoqT0cLlBU8IgofwV70I9o4QD7qoyKqZzlruxr8O1c8VglruxImSYwHlElrlb7roDKqom","https:\\/\\/mixer.mobon.net\\/view?sspNo=1897&dspTpCode=18&pltfomTpCode=03&userId=adserver&impId=2deb4fd7-02fa-4ace-9977-f90820c4365c&abusingCode=3JGKSu9jjNZnkTgs6mmioQ&krwPoint=24.699342727661133&point=0.01735967254638672&burl=N"],"adClickTrackers":["https:\\/\\/mixer.mobon.net\\/click?sspNo=1897&dspTpCode=18&pltfomTpCode=03&userId=adserver&impId=2deb4fd7-02fa-4ace-9977-f90820c4365c&abusingCode=3JGKSu9jjNZnkTgs6mmioQ"],"adDesc":"리워드앱재택 부업 찾는다면? 무료 앱 프리캐시로 쉽고 재밌게 리워드 받기\\n","productInfo":{"pCode":"","pName":"Freecash - 돈버는앱프리캐시","pPrice":"","pDcRate":0,"pDcPrice":"","pDeliveryYn":""}}]}}',
   };
 };
