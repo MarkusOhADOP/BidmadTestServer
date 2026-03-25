8c99352a9695c0a19b38cc0420c3ba003f69ee57
MarkusOhADOP <83572705+MarkusOhADOP@users.noreply.github.com>
2026-03-25 14:33:23 +0900

    가시성 테스트용 항상 실패/성공 엔드포인트 추가

---
+++ netlify/functions/vis-test-always-fail.js
@@ -0,0 +1,28 @@
+// Always returns a fake ad network that will fail to load.
+// Used for visibility accumulation testing (failure retry flow).
+export const handler = async () => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      command: "ok",
+      isFBB: "N",
+      status: "200",
+      type: "banner",
+      ads: {
+        "vis-test-always-fail": {
+          adcode: "vis-test-always-fail",
+          pubid: "",
+          passback: "",
+          adtype: "testtest-test-test-test-testtesttest",
+          advidx: "09316c92-262a-463f-a73b-1f1c34a6de93",
+          areaidx: "vis-test-always-fail",
+          adweight: "100",
+          adv_price: "10",
+          order: "1",
+          width: "320",
+          height: "50",
+        },
+      },
+    }),
+  };
+};
---
+++ netlify/functions/vis-test-always-success.js
@@ -0,0 +1,28 @@
+// Always returns a valid AdMob test banner that will load successfully.
+// Used for visibility accumulation testing (fail→success transition).
+export const handler = async () => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      command: "ok",
+      isFBB: "N",
+      status: "200",
+      type: "banner",
+      ads: {
+        "vis-test-always-success": {
+          adcode: "ca-app-pub-3940256099942544/2435281174",
+          pubid: "",
+          passback: "",
+          adtype: "ce56da00-1a18-11e9-9ed2-02c31b446301",
+          advidx: "23503470-4e23-4a84-a77b-a9fe25deb784",
+          areaidx: "vis-test-always-success",
+          adweight: "100",
+          adv_price: "1",
+          order: "1",
+          width: "320",
+          height: "50",
+        },
+      },
+    }),
+  };
+};
