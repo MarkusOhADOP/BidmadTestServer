040fe0979388ba7f1b119e84bf4e556064476be5
MarkusOhADOP <83572705+MarkusOhADOP@users.noreply.github.com>
2026-04-22 15:33:59 +0900

    Re-add targetpick-reward mock (rewarded adapter restored with UIView containment)

---
+++ netlify/functions/targetpick-reward.js
@@ -0,0 +1,31 @@
+// TargetPick (메조미디어 LibADPlus) rewarded video mock.
+// adcode is the composite placement: "publisherID|mediaID|sectionID"
+// (sample-app pid 102/202/804408 from bitbucket.org/mezzomediaadtech/ios-v300-swift).
+// The adapter wraps ADMZVideoView in a custom modal VC and fires rewarded on
+// ADMZResponseStatusType.VideoAdComplete (code 309).
+export const handler = async () => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      command: "ok",
+      isFBB: "N",
+      status: "200",
+      type: "reward",
+      ads: {
+        "c3d4e5f6-tpck-4000-8000-rewardvideo": {
+          adcode: "102|202|804408",
+          pubid: "",
+          passback: "",
+          adtype: "63022682-ca98-49e2-a211-f62288ca315e",
+          advidx: "63022682-ca98-49e2-a211-f62288ca315e",
+          areaidx: "c3d4e5f6-tpck-4000-8000-rewardvideo",
+          adweight: "100",
+          adv_price: "1",
+          order: "1",
+          width: "1",
+          height: "1",
+        },
+      },
+    }),
+  };
+};
